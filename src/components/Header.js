8b0c197fb7a46f50e88a108de06098fd7865ee1f
Mechanical-Coder1 <jakkulaprasaad@gmail.com>
2023-10-30 14:33:56 +0530

    Change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
-import {useState} from "react"
-import { LOGO_URL } from "../utils/constants"
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
-    const [login, setLogin] = useState("Login")
-    return(
-        <div className='header-container'>
-            <div>
-                <img className='logo' src={LOGO_URL}></img>
-            </div>
-            <h1 >LUCKY BAR & RESTAURENT</h1>
-            <div className='nav-items'>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                    <button className="login-btn">Login</button>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  const [login, setLogin] = useState("Login");
+  return (
+    <div className="header-container">
+      <div>
+        <img className="logo" src={LOGO_URL}></img>
+      </div>
+      <h1>LUCKY BAR & RESTAURENT</h1>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              login === "Login" ? setLogin("Logout") : setLogin("Login");
+            }}
+          >
+            {login}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
